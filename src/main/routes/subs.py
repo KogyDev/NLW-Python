61d2ebd79cf96a73b9149682a3879e9e4301cfac
Matheus Cornedi <matheus.r.cornedi@gmail.com>
2025-02-21 23:02:00 -0300

    Update subs.py

--- src/main/routes/subs.py
+++ src/main/routes/subs.py
@@ -1,26 +1,26 @@
 from flask import Blueprint, jsonify, request
 from src.http_types.http_request import HttpRequest
-from src.model.repositories.eventos_repository import EventosRepository
-from src.main.use_cases.events_creator import EventsCreator  # Certifique-se de importar corretamente
-from src.validators.events_creator_validator import events_creator_validator  # Se essa função existir
+from src.model.repositories.subscribers_repository import SubscribersRepository
+from src.controllers.subscriber.subscribers_creator import SubscribersCreator
+from src.validators.subscribers_creator_validator import subscribers_creator_validator  # Se existir
 
-# Criando o Blueprint com o nome correto
+
 subs_route_bp = Blueprint("subs_route", __name__)
 
-@subs_route_bp.route("/subscriber", methods=["POST"])  # Corrigido de event_route_bp para subs_route_bp
+@subs_route_bp.route("/subscriber", methods=["POST"])
 def create_new_subs():
-    # Validação da requisição
-    events_creator_validator(request)
     
-    # Criando um HttpRequest com o corpo da requisição
+    subscribers_creator_validator(request)
+    
+    
     http_request = HttpRequest(body=request.json)
+
     
-    # Criando instâncias necessárias
-    eventos_repo = EventosRepository()
-    events_creator = EventsCreator(eventos_repo)
+    subs_repo = SubscribersRepository()
+    subs_creator = SubscribersCreator(subs_repo)
+
     
-    # Chamando a função create apenas uma vez
-    http_response = events_creator.create(http_request)
+    http_response = subs_creator.create(http_request)
+
     
-    # Retornando a resposta formatada
     return jsonify(http_response.body), http_response.status_code

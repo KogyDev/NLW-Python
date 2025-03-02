42c0d20b1e1306e9a7e834b36e937a423b4ce665
Matheus Cornedi <matheus.r.cornedi@gmail.com>
2025-03-02 17:54:11 -0300

    Update subscribers_repository.py

--- src/model/repositories/interfaces/subscribers_repository.py
+++ src/model/repositories/interfaces/subscribers_repository.py
@@ -1,10 +1,14 @@
-from src.model.entities.inscritos import Inscritos 
+from src.model.entities.inscritos import Inscritos
 from abc import ABC, abstractmethod
 
-class SubscribersRepository(ABC):
+
+class SubscribersRepositoryInterface(ABC):
+
     @abstractmethod
-    def insert(self, subscriber_infos: dict) -> None: pass
-     
+    def insert(self, subscriber_infos: dict) -> None:
+        pass
+
     @abstractmethod
-    def select_subscriber(self, email: str, event_id: int) -> Inscritos: pass
+    def select_subscriber(self, email: str, evento_id: int) -> Inscritos:
+        pass
 
